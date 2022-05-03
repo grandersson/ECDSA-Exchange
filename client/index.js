1fc6d89bbb9ae3635aa3e88e2df754b514e84574
Greg <grandersson1991@gmail.com>
2022-05-03 18:23:38 -0400

    added key and balance verification features

--- client/index.js
+++ client/index.js
@@ -1,5 +1,9 @@
 import "./index.scss";
 
+const {keccak_256} = require('@noble/hashes/sha3');
+const { bytesToHex } = require('@noble/hashes/utils');
+const secp = require('@noble/secp256k1');
+
 const server = "http://localhost:3042";
 
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
@@ -15,10 +19,52 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   });
 });
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
+document.getElementById("transfer-amount").addEventListener('click', async () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const keySubmit = document.querySelector("#private-key").value;
+  const accountBalance = document.querySelector("#balance").textContent;
+  const sendBtn = document.querySelector("#transfer-amount");
+
+  const myMessageHash = bytesToHex((keccak_256(JSON.stringify({ sender, amount, recipient }))));
+  console.log(myMessageHash);
+  const signature = await secp.sign(myMessageHash, keySubmit, {recovered: true});
+  console.log(signature);
+  console.log(signature[0]);
+  console.log(signature[1]);
+
+  const signedSignature = signature[0];
+  const recoveryKey = signature[1];
+
+  const recoverPub = secp.recoverPublicKey(myMessageHash, signedSignature, recoveryKey);
+  console.log(recoverPub);
+  const recoveredKeyHex = Buffer.from(recoverPub).toString('hex');
+  console.log(recoveredKeyHex);
+  const keyMatch = "0x" + recoveredKeyHex.slice(recoveredKeyHex.length - 40);
+  console.log(keyMatch);
+
+  // check if public key matches address key
+  const isValid = (key1, key2) => {
+    if (key1 == key2) {
+      return true;
+    } else return false;
+  }
+
+  // verify that account has enough funds for transaction
+  const hasBalance = (sendAmt, funds) => {
+    sendAmt = Number.parseInt(sendAmt, 10);
+    funds = Number.parseInt(funds, 10);
+    if (sendAmt <= funds) {
+      return true;
+    } else { return false };
+  }
+
+  console.log(hasBalance(amount, accountBalance));
+
+  console.log(isValid(sender, keyMatch));
+
+  if ((isValid(sender,keyMatch)) && (hasBalance(amount, accountBalance))) {
 
   const body = JSON.stringify({
     sender, amount, recipient
@@ -31,4 +77,11 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   }).then(({ balance }) => {
     document.getElementById("balance").innerHTML = balance;
   });
+  sendBtn.classList.add("success");
+  setTimeout(() => {
+    sendBtn.classList.remove("success");
+  }, 3500);
+} else {
+    alert('Wrong Key or Insufficient Funds!');
+  };
 });
--- server/index.js
+++ server/index.js
@@ -2,17 +2,61 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const {keccak_256} = require('@noble/hashes/sha3');
+const { bytesToHex } = require('@noble/hashes/utils');
+const secp = require('@noble/secp256k1')
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const privKeyArr = [];
+
+// create private key
+const createPvt = () => {
+  let privateKey = secp.utils.randomPrivateKey();
+  privateKey = Buffer.from(privateKey).toString('hex');
+  return privateKey;
 }
+
+// get public key
+const createPub = (privKey) => {
+  let publicKey = secp.getPublicKey(privKey);
+  publicKey = Buffer.from(publicKey).toString('hex');
+  publicKey = "0x" + publicKey.slice(publicKey.length - 40);
+  return publicKey;
+}
+
+// assign random balance
+const genBalance = () => {
+  let balance = Math.floor(Math.random() * 9999 + 1)
+  return balance;
+}
+
+const balances = {}
+
+// create n number of accounts
+const createAccounts = (num) => {
+  for (let i = 0; i < num; i++) {
+    let accountNumber = i;
+    console.log(`Account Number: ${accountNumber}`);
+    let privKey = createPvt();
+    console.log(`Private Key: ${privKey}`);
+    let pubKey = createPub(privKey);
+    console.log(`Public Key: ${pubKey}`);
+    let accBalance = genBalance();
+    console.log(`Balance: ${accBalance}`);
+    balances[pubKey] = accBalance;
+  }
+};
+
+// one for everyone in the class
+createAccounts(51);
+
+console.log(Object.entries(balances));
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
